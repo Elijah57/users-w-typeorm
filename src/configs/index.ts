92706145fda0670a5eacd6e40a4da09b62cd44f1
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-07-27 13:11:11 +0100

    dev: update config file

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
+    HOST: process.env.HOST,
     DB_USER: process.env.DB_USERNAME,
     DB_PWD: process.env.DB_PWD,
     DB_HOST: process.env.DB_HOST,
@@ -10,6 +11,14 @@ const config = {
     DB_PORT: Number(process.env.DB_PORT),
 
     PORT: Number(process.env.PORT),
+    JWT_SECRET: process.env.JWT_SECRET,
+
+
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: Number(process.env.SMTP_PORT),
+    SMTP_SERVICE: process.env.SMTP_SERVICE,
+    SMTP_PASS: process.env.SMTP_PASS,
+    SMTP_MAIL: process.env.SMTP_MAIL
 
 }
 
